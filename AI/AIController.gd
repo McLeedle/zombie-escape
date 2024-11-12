944bd26096dd3749323e2b4b3700a132c82be4d8
Unkept <mmojunior@hotmail.com>
2024-11-11 21:37:57 -0800

    Initial Commit. I started working but didn't think to start github to track my progress.

---
+++ AI/AIController.gd
@@ -0,0 +1,108 @@
+class_name AIController
+extends CharacterBody3D
+
+@export var walk_speed : float = 1.0
+@export var run_speed : float = 2.5
+var running : bool = false
+var is_stopped : bool = false
+var look_at_player : bool = false
+
+var move_direction : Vector3
+var target_y_rot : float
+
+@onready var agent : NavigationAgent3D = get_node("NavigationAgent3D")
+@onready var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var player : PlayerController = get_tree().get_nodes_in_group("Player")[0]
+@onready var anim_player : AnimationPlayer = get_node("AnimationPlayer")
+@onready var raycast : RayCast3D = get_node("RayCast3D")
+
+var player_distance : float
+var stuck_timer : float = 0.0
+var stuck_threshold : float = 1.0
+
+func _process(delta):
+	if player:
+		player_distance = position.distance_to(player.position)
+
+func _physics_process(delta):
+	# If the AI's velocity is small or zero, increase the stuck timer
+	if velocity.length() < 0.1:
+		stuck_timer += delta
+	else:
+		stuck_timer = 0
+	
+	# check for obstacles using raycasting
+	if raycast.is_colliding():
+		stuck_timer += delta
+	
+	# If the AI hasn't moved for a while (and is colliding), handle stuck behavior
+	if stuck_timer > stuck_threshold:
+		handle_stuck_behavior()
+		
+	# Stop the ai if player is far away.
+	#if player_distance > 30:
+	#	return
+	
+	# Apply gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	var target_pos = agent.get_next_path_position()
+	var move_dir = global_position.direction_to(target_pos)
+	move_dir.y = 0
+	move_dir = move_dir.normalized()
+	
+	# Don't move if we're done with navigation or is stopped.
+	if agent.is_navigation_finished() or is_stopped:
+		move_dir = Vector3.ZERO
+	
+	var current_speed = walk_speed
+	
+	if running:
+		current_speed = run_speed
+	
+	# Set velocity.
+	velocity.x = move_dir.x * current_speed
+	velocity.z = move_dir.z * current_speed
+	
+	# Rotate to velocity or player.
+	if !look_at_player and velocity.length() > 0:
+		target_y_rot = atan2(velocity.x, velocity.z)
+	elif look_at_player:
+		var player_dir = (player.position - position)
+		target_y_rot = atan2(player_dir.x, player_dir.z)
+	
+	rotation.y = lerp_angle(rotation.y, target_y_rot, 0.1)
+	
+	move_and_slide()
+
+func handle_stuck_behavior():
+	print("AI is stuck! Resetting path...")
+	reset_navigation()
+	
+func reset_navigation():
+	# Calculate new path
+	var new_target = get_new_target_position()
+	move_to_position(new_target)
+	
+func get_new_target_position() -> Vector3:
+	var random_offset = Vector3(randf_range(-5,5), 0, randf_range(-5,5))
+	var new_position = player.position + random_offset
+	return new_position
+	
+func move_to_position (to_position : Vector3, adjust_pos : bool = true):
+	if not agent:
+		agent = get_node("NavigationAgent3D")
+		
+	is_stopped = false
+	
+	var map = get_world_3d().navigation_map
+	var adjusted_to_position = NavigationServer3D.map_get_closest_point(map, to_position)
+	agent.target_position = adjusted_to_position
+	agent.target_position = to_position
+
+func play_walk_animation():
+	anim_player.play("Walk")
+
+func play_run_animation():
+	anim_player.play("Run")
---
+++ Player Controller/Pausing/Pause.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@onready var pause_panel : Panel = get_node("Panel")
+
+var is_paused : bool = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_pause()
+		
+func toggle_pause() -> void:
+	if is_paused:
+		unpause()
+	else:
+		pause()
+
+func pause():
+	is_paused = true
+	pause_panel.visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	Engine.time_scale = 0
+
+func unpause():
+	is_paused = false
+	pause_panel.visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Engine.time_scale = 1
+
+func _on_resume_button_pressed() -> void:
+	unpause()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
